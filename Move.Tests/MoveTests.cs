fd388fbaa95658d36d5a2b396eec25cfe30995bf
Dmitry-Laptop <aleshkindima25@gmail.com>
2022-05-18 13:36:11 +0600

    Добавлены тесты на Move

---
+++ Move.Tests/MoveTests.cs
@@ -0,0 +1,182 @@
+using System;
+using Xunit;
+using Moq;
+
+
+namespace Move.Tests
+{
+    public class MoveTests
+    {
+        [Fact]
+        public void StartMove()
+        {
+            bool IsChangeVelocityOkay = true;
+            bool IsMovementOkay = true;
+            bool IsGetByIDOkay = true;
+       
+            var m = new Mock<MoveStartable>();
+            var v = new Mock<VelocityChangeable>();
+            var obj_move = m.Object;
+            var obj_vel = v.Object;
+            var Velocity = new Mock<ChangeVelocityCmd>();
+            var Movement = new Mock<Command>();
+            var ID = new Mock<Command>();
+
+            StartMove c = new StartMove(obj_move);
+
+            Func<string, object[], object> strategy = (key, args) =>
+            {
+                if (key == "GameObject.ChangeVelocity")
+                {
+                    if(args.Length != 2)
+                    {
+                        IsChangeVelocityOkay = false;
+                    }
+
+                    if(!(args[0] is MoveStartable))
+                    {
+                        IsChangeVelocityOkay = false;
+                    }
+
+                    if(!(args[1] is string))
+                    {
+                        IsChangeVelocityOkay = false;
+                    }
+
+                    return Velocity.Object;
+                }
+
+                else if (key == "GameObject.Movement")
+                {
+                    if (args.Length != 1)
+                    {
+                        IsMovementOkay = false;
+                    }
+
+                    if (!(args[0] is MoveStartable))
+                    {
+                        IsMovementOkay = false;
+                    }
+
+                    return Movement.Object;
+                }
+
+                else if (key == "Queue")
+                {
+                    var Q = new Mock<Queue>();
+                    return Q.Object;
+                }
+
+                else if (key == "GameObject.GetByID")
+                {
+                    if (args.Length != 1)
+                    {
+                        IsGetByIDOkay = false;
+                    }
+
+                    if (!(args[0] is string))
+                    {
+                        IsGetByIDOkay = false;
+                    }
+                    
+                    return ID.Object;
+                }
+
+                else
+                {
+                    throw new Exception();
+                }
+            };
+
+            Assert.True(IsChangeVelocityOkay);
+            Assert.True(IsMovementOkay);
+            Assert.True(IsGetByIDOkay);
+        }
+
+        [Fact]
+        public void Move()
+        {
+            var m = new Mock<Movable>();
+            m.Setup(m => m.Velocity).Returns(new Vector(5,2)).Verifiable();
+            m.Setup(m => m.Position).Returns(new Vector(6,6)).Verifiable();
+            m.SetupSet(m => m.Position = It.Is<Vector> (p => p.Equals(new Vector(11,8)))).Verifiable();
+            var c = new Move(m.Object);
+            c.Execute();
+            m.VerifyAll();
+        }
+
+        [Fact]
+        public void StopMove()
+        {
+
+            bool GetByIDisOkay = true;
+            bool VelocityRemoveisOkay = true;
+            bool EmptyCommandSetisOkay = true;
+
+            var ID = new Mock<Command>();
+            var Movement = new Mock<Command>();
+
+            Func<string, object[], object> strategy = (key, args) =>
+            {
+                if (key == "GameObject.GetByID")
+                {
+                    if (args.Length != 1)
+                    {
+                        GetByIDisOkay = false;
+                    }
+
+                    if(!(args[0] is string))
+                    {
+                        GetByIDisOkay = false;
+                    }
+
+                    return ID.Object;
+                }
+
+                else if (key == "GameObject.Velocity.Remove")
+                {
+                    if (args.Length != 1)
+                    {
+                        VelocityRemoveisOkay = false;
+                    }
+
+                    if (!(args[0] is StopMovable))
+                    {
+                        VelocityRemoveisOkay = false;
+                    }
+
+                    return Movement.Object;
+                }
+
+                else if (key == "GameObject.EmptyCommand.Set")
+                {
+                    if (args.Length != 2)
+                    {
+                        EmptyCommandSetisOkay = false;
+                    }
+
+                    if (!(args[0] is StopMovable))
+                    {
+                        EmptyCommandSetisOkay = false;
+                    }
+
+                    if (!(args[1] is EmptyCommand))
+                    {
+                        EmptyCommandSetisOkay = false;
+                    }
+
+                    return new EmptyCommand();
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            };
+
+            Assert.True(GetByIDisOkay);
+            Assert.True(VelocityRemoveisOkay);
+            Assert.True(EmptyCommandSetisOkay);
+        }
+    }
+
+}
